5442ce61d27aee17f819ca13f1a3553523665d64
nopead <smirnov.smirnov2003@yandex.ru>
2025-02-05 16:48:16 +0300

    added examples from subchapter "Legacy classes and interfaces"

---
+++ chapter19/javas/PhoneBook.java
@@ -0,0 +1,68 @@
+package chapter19.classes;
+
+import java.util.Properties;
+import java.io.BufferedReader; 
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.FileNotFoundException; 
+import java.io.File;
+
+class Phonebook{
+	public static void main(String[] args) throws IOException{
+		Properties ht = new Properties();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String name, number;
+		File phonebookFile = null;
+		FileInputStream fin = null;
+		boolean changed = false;
+		
+		try{
+			phonebookFile = new File("chapter19/assets/phonebook.dat");
+			fin = new FileInputStream(phonebookFile);
+		}catch(FileNotFoundException e){
+			
+		}
+		
+		try{
+			if (fin != null){
+				ht.load(fin);
+				fin.close();
+			}
+			else{}
+		} catch(IOException e){
+			System.out.println("Ошибка чтения файла");
+		}
+		
+		do{
+			System.out.println("Введите имя. 'exit' для завершения");
+			name = br.readLine();
+			if (name.equals("exit")){
+				continue;
+			}
+			System.out.println("Введите номер:");
+			number = br.readLine();
+			ht.put(name, number);
+			changed = true;
+		} while(!name.equals("exit"));
+		
+		if (changed) {
+			phonebookFile = new File("chapter19/assets/phonebook.dat");
+			FileOutputStream fout = new FileOutputStream(phonebookFile);
+			ht.store(fout, "Телефонная книга");
+			fout.close();
+		}
+		
+		do {
+			System.out.println("Введите имя для поиска. 'exit' для выхода ");
+			name = br.readLine();
+			if (name.equals("exit")){
+				continue;
+			}
+			number = (String)ht.get(name);
+			System.out.println("Номер телефона " + name + ": " + number);
+		} while (!name.equals("exit"));
+	}
+}
---
+++ chapter19/javas/StackDemo.java
@@ -0,0 +1,37 @@
+package chapter19.classes;
+
+import java.util.Stack;
+import java.util.EmptyStackException;
+
+class StackDemo{
+	static void showPush(Stack<Integer> st, int value){
+		st.push(value);
+		System.out.println("pushed " + value + " in stack");
+		System.out.println("current stack: " + st);
+	}
+	
+	static void showPop(Stack<Integer> st){
+		int a = st.pop();
+		System.out.println("popped " + a + " from stack");
+		System.out.println("current stack: " + st);
+	}
+	
+	public static void main(String[] args){
+		Stack<Integer> st = new Stack<Integer>();
+
+		System.out.println("stack: " + st);
+		
+		showPush(st, 42);
+		showPush(st, 66);
+		showPush(st, 99);
+		showPop(st);
+		showPop(st);
+		showPop(st);
+		
+		try{
+			showPop(st);
+		} catch(EmptyStackException e){
+			System.out.println("Пустой стек. Ошибка: " + e);
+		}
+	}
+}
