ab1786967322c528b83cd107859af96619b14f8e
nopead <smirnov.smirnov2003@yandex.ru>
2025-02-11 16:40:20 +0300

    added rim, tire variables to Gear class, added method gear_inches()

--- sandymetzruby/chapter2/javas/Main.java
+++ sandymetzruby/chapter2/javas/Main.java
@@ -4,12 +4,12 @@ import sandymetzruby.chapter2.classes.Gear;
 
 class Main{
 	public static void main(String[] args){
-		Gear first_gear = new Gear(52, 11);
-		Gear second_gear = new Gear(30, 27);
+		Gear first_gear = new Gear(52, 11, 26, 1.5);
+		Gear second_gear = new Gear(30, 27, 24, 1.25);
 		
-		System.out.println("first_gear ratio:");
-		first_gear.ratio();
-		System.out.println("second_gear ratio:");
-		second_gear.ratio();
+		System.out.print("first_gear gear_inches: ");
+		System.out.print(first_gear.gear_inches() + "\n");
+		System.out.print("second_gear gear_inches: ");
+		System.out.print(second_gear.gear_inches() + "\n");
 	}
 }
