4f86ebd2be206014c9cdd93bdeede15d7ab9658a
nopead <smirnov.smirnov2003@yandex.ru>
2025-02-12 10:37:17 +0300

    changed gearInches method in class Gear

--- sandymetzruby/chapter2/javas/Gear.java
+++ sandymetzruby/chapter2/javas/Gear.java
@@ -4,10 +4,19 @@ class Gear{
 	
 	private int chairing;
 	private int cog;
+	private double rim;
+	private double tire;
+	
+	Gear(int chairing, int cog){
+		this.chairing = chairing;
+		this.cog = cog;
+	}
 	
 	Gear(int chairing, int cog, double rim, double tire){
 		this.chairing = chairing;
 		this.cog = cog;
+		this.rim = rim;
+		this.tire = tire;
 	}
 	
 	public int getChairing(){
@@ -17,7 +26,6 @@ class Gear{
 	public int getCog(){
 		return cog;
 	}
-	
 	
 	public int getCog(int unanticipated_adjustment_factor){
 		return cog * unanticipated_adjustment_factor;
@@ -31,7 +39,23 @@ class Gear{
 		return cog * (foo() ? bar_adjustment : baz_adjustment);
 	}
 	
+	public double getRim(){
+		return rim;
+	}
+	
+	public double getTire(){
+		return tire;
+	}
+	
 	public double ratio(){
 		return(chairing * 1.0 / cog);
 	}
+	
+	public double diameter(){
+		return rim + (tire * 2);
+	}
+	
+	public double gearInches(){
+		return ratio() * diameter();
+	}	
 }
--- sandymetzruby/chapter2/javas/Main.java
+++ sandymetzruby/chapter2/javas/Main.java
@@ -7,13 +7,13 @@ import java.util.ArrayList;
 
 class Main{
 	public static void main(String[] args){
-		Gear first_gear = new Gear(52, 11, 26, 1.5);
-		Gear second_gear = new Gear(30, 27, 24, 1.25);
+		Gear firstGear = new Gear(52, 11, 26, 1.5);
+		Gear secondGear = new Gear(30, 27, 24, 1.25);
 		
-		System.out.print("first_gear ratio: ");
-		System.out.print(first_gear.ratio() + "\n");
-		System.out.print("second_gear ratio: ");
-		System.out.print(second_gear.ratio() + "\n");
+		System.out.print("firstGear gearInches(): ");
+		System.out.print(firstGear.gearInches() + "\n");
+		System.out.print("secondGear gearInches(): ");
+		System.out.print(secondGear.gearInches() + "\n");
 		
 		/*System.out.print("first_gear gear_inches: ");
 		System.out.print(first_gear.gear_inches() + "\n");
